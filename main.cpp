1821dc00d0fa1338ff2358f10aa8f807e5665d0a
stef1261 <stefan.address@gmail.com>
2015-09-07 18:08:05 +0100

    Main.cpp added
    The code to make this program function is in this file. It takes in two
    numbers, a beginning, and an end to calculate Gauss' number from.

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int gaussNumber(int current_number, int index, int last_number)
+{
+    /*
+    if (index <= last_number)
+    {
+        current_number += current_number + 1;
+        index++;
+    }
+    return gaussNumber(current_number, index, last_number);
+    */
+    while(index < last_number)
+    {
+        index++;
+        current_number += index;
+    }
+    return current_number;
+}
+
+int main()
+{
+    cout << "Enter your starting number: ";
+    int firstNumber = 0;
+    cin >> firstNumber;
+
+    cout << "Enter your last number: ";
+    int lastNumber = 0;
+    cin >> lastNumber;
+
+    cout << gaussNumber(firstNumber, firstNumber, lastNumber);
+
+    cin.get();
+    cin.ignore();
+
+    return 0;
+}
